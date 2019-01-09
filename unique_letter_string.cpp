18487674a59cdfed7a4cfad78053f275009e43a9
Fenr1r <40751502+fenrir94@users.noreply.github.com>
2019-01-09 11:43:59 +0900

    Create unique_letter_string.cpp
    solving No.828 Unique Letter String. Not Accepted

---
+++ unique_letter_string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int uniqueLetterString(string S) {
+        vector<char> subs;
+        int count=0;
+        for(int i=0; i< S.length();++i){
+            subs.push_back(S[i]);
+            count = count + calunique(subs, 0) + recursive(subs, i+1, S, 1);
+            subs.pop_back();
+        }
+        return count;
+    } 
+    
+    int recursive(vector<char>& subs, int next, string s, int precount)
+    {
+        int count = 0;
+        if(next < s.length())
+        {
+            subs.push_back(s[next]);
+            count = calunique(subs, precount);
+            count = count + recursive(subs, next+1, s, count);
+            subs.pop_back();
+        }
+        return count;
+    }
+    
+    int calunique(vector<char>& subs, int precount){
+        int count = precount;
+        for(int i=0; i< subs.size()-1;++i)
+        {
+            if(subs[i]==subs.back())
+            {
+                count--;
+                if(count <0) count = 0;
+                return count;
+            }
+        }
+        count++;
+        return count;
+    }
+};
