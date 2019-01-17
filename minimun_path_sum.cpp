dc2c3cbd9b04de1b97ca7c8038ef7d35b886d157
Fenr1r <40751502+fenrir94@users.noreply.github.com>
2019-01-17 10:35:12 +0900

    Create minimun_path_sum.cpp
    Solve No.64 Minimun Path Sum.

---
+++ minimun_path_sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> map(m, vector<int>(n,0));
+        for(int i=0; i<m;++i)
+        {
+            for(int j=0; j< n;++j)
+            {
+                if(i==0 && j==0) map[i][j]=grid[i][j];
+                else if(i == 0 && j !=0)
+                {
+                    map[i][j] = map[i][j-1]+grid[i][j];
+                }
+                else if(i !=0 && j==0)
+                {
+                    map[i][j] = map[i-1][j]+grid[i][j];
+                }
+                else{
+                    map[i][j] = min(map[i][j-1],map[i-1][j])+grid[i][j];
+                }
+            }
+        }
+        return map[m-1][n-1];
+        
+    }
+};
